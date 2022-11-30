b584fabeead5feaf7f2fa4b383ecfd5acf219760
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-11-30 22:54:26 +0100

    refactor: rename assert_no_error to assert

--- src/hocon_tconf.erl
+++ src/hocon_tconf.erl
@@ -129,7 +129,7 @@ translate(Schema, Conf, Mapped) ->
                 )
              || N <- Namespaces
             ]),
-            ok = assert_no_error(Schema, Res),
+            ok = assert(Schema, Res),
             %% rm field if translation returns undefined
             [{K, V} || {K, V} <- lists:ukeymerge(1, Res, Mapped), V =/= undefined]
     end.
@@ -170,7 +170,7 @@ assert_integrity(Schema, Conf0, #{format := Format}) ->
         end,
     Names = hocon_schema:validations(Schema),
     Errors = assert_integrity(Schema, Names, Conf, []),
-    ok = assert_no_error(Schema, Errors).
+    ok = assert(Schema, Errors).
 
 assert_integrity(_Schema, [], _Conf, Result) ->
     lists:reverse(Result);
@@ -292,7 +292,7 @@ map(Schema, Conf0, Roots0, Opts0) ->
     Conf2 = filter_by_roots(Opts, Conf1, Roots),
     Conf = apply_envs(Schema, Conf2, Opts, Roots),
     {Mapped0, NewConf} = do_map(Roots, Conf, Opts, ?MAGIC_SCHEMA),
-    ok = assert_no_error(Schema, Mapped0),
+    ok = assert(Schema, Mapped0),
     ok = assert_integrity(Schema, NewConf, Opts),
     Mapped = log_and_drop_env_overrides(Opts, Mapped0),
     {Mapped, maybe_convert_to_plain_map(NewConf, Opts)}.
@@ -1137,7 +1137,7 @@ drop_nulls(Opts, Map) when is_map(Map) ->
         Map
     ).
 
-assert_no_error(Schema, List) ->
+assert(Schema, List) ->
     case find_errors(List) of
         ok ->
             ok;
